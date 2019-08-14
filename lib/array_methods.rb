32f4135d2c75ee514ff30125d2b63a06f74eca53
cedric e goddard <8ceg6@comcast.net>
2019-08-14 19:22:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def using_include(array, element)
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
     
-    array.include?('wow')and
-    array.include?('Tardis')
+    array.include?("wow")and
+    array.include?("Tardis")
     
 end
 
